97fe33012dcb9db835533c4791f1fdb781114722
NgocZ <ngngngoc2101@gmail.com>
2019-06-15 00:05:32 +0700

    màn hình đăng kí

---
+++ app/src/main/java/com/example/nhom10_doan/register_activity.java
@@ -0,0 +1,13 @@
+package com.example.nhom10_doan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class register_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
