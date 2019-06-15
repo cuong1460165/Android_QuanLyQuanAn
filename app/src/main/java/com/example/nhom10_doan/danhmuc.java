ecbe6986bb70934caf66e8d2e18574c0c63d67ee
cuong <cuongnguyen09111996@gmail.com>
2019-06-15 23:02:56 +0700

    Danh Muc Mon An

---
+++ app/src/main/java/com/example/nhom10_doan/danhmuc.java
@@ -0,0 +1,13 @@
+package com.example.nhom10_doan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class danhmuc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danhmuc);
+    }
+}
