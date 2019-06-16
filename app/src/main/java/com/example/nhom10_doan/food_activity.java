110af3fa14d39ac773c04aa6985b1a440cec28ad
Nguyen Duong <nnbduong@gmail.com>
2019-06-16 07:31:01 +0700

    điều chỉnh chức năng load món ăn
    điều chỉnh chức năng load món ăn

--- app/src/main/java/com/example/nhom10_doan/food_activity.java
+++ app/src/main/java/com/example/nhom10_doan/food_activity.java
@@ -7,35 +7,64 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class food_activity extends AppCompatActivity {
-    String[] name = {"Gỏi bò", "Ức vịt sốt rượu vang ",
-            "Bồ câu tiềm bí đỏ", "Gà nướng",
-            "Bò lá lốt", "Heo tộc quay lu chặt mẹt",
-            "Cá tầm nướng muối ớt", "Cánh gà chiên mật ong",
-            "Gói cuốn tôm thịt", "Cá chình nướng muối ớt"};
-    String[] dec = {"Gỏi bò", "Ức vịt sốt rượu vang ",
-            "Bồ câu tiềm bí đỏ", "Gà nướng",
-            "Bò lá lốt", "Heo tộc quay lu chặt mẹt",
-            "Cá tầm nướng muối ớt", "Cánh gà chiên mật ong",
-            "Gói cuốn tôm thịt", "Cá chình nướng muối ớt"};
-    int[] image = {R.drawable.h1, R.drawable.h2,
-            R.drawable.h3, R.drawable.h4,
-            R.drawable.h5, R.drawable.h6,
-            R.drawable.h7, R.drawable.h8,
-            R.drawable.h9, R.drawable.h10};
+    ArrayList<String> name = new ArrayList();
+    ArrayList<String> dec = new ArrayList();
+    ArrayList<Integer> image = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        initData();
+
+
         GridView gridView = (GridView) findViewById(R.id.foodGV);
         GridViewAdapter_Food gridViewAdapter = new GridViewAdapter_Food(this, name, image, dec);
         gridView.setAdapter(gridViewAdapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(food_activity.this, dec[position], Toast.LENGTH_SHORT).show();
+                Toast.makeText(food_activity.this, dec.get(position), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void initData(){
+        name.add("Goi bo");
+        name.add("Uc vit sot ruou vang");
+        name.add("Bo cau tiem bi do");
+        name.add("Ga nuong");
+        name.add("Bo la lot");
+        name.add("Heo toc quay lu chat met");
+        name.add("Ca tam nuong muoi ot");
+        name.add("Canh ga chien mat ong");
+        name.add("Goi cuon tom thit");
+        name.add("Ca chinh nuong muoi ot");
+
+        dec.add("Goi bo");
+        dec.add("Uc vit sot ruou vang");
+        dec.add("Bo cau tiem bi do");
+        dec.add("Ga nuong");
+        dec.add("Bo la lot");
+        dec.add("Heo toc quay lu chat met");
+        dec.add("Ca tam nuong muoi ot");
+        dec.add("Canh ga chien mat ong");
+        dec.add("Goi cuon tom thit");
+        dec.add("Ca chinh nuong muoi ot");
+
+        image.add(R.drawable.h1);
+        image.add(R.drawable.h2);
+        image.add(R.drawable.h3);
+        image.add(R.drawable.h4);
+        image.add(R.drawable.h5);
+        image.add(R.drawable.h6);
+        image.add(R.drawable.h7);
+        image.add(R.drawable.h8);
+        image.add(R.drawable.h9);
+        image.add(R.drawable.h10);
     }
 }
